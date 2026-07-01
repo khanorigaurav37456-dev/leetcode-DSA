23c4f949ab64e89031d94b64e56a05b00b372578
Gaurav <khanorigaurav37456@gmail.com>
2026-07-01 16:01:50 +0530

    Time: 1 ms (19.05%), Space: 46.8 MB (30.1%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+        public int LengthOfCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                //calculate the length
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        int length = 0;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=null && fast.next!=null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = LengthOfCycle(slow);
+                break;
+            }
+        }
+
+            // find the start node of cycle
+            if(length <=0){
+                return null;
+            }
+            ListNode f = head;
+            ListNode s = head;
+
+            while(length > 0){
+                s = s.next;
+                length--;
+            }
+
+            while(f != s){
+                f= f.next;
+                s= s.next;
+            }
+
+        return f;
+    }
+}
