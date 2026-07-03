ff03d58640a3a26cf9e23d4cb65aebea9bb9e9e3
Gaurav <khanorigaurav37456@gmail.com>
2026-07-03 12:44:45 +0530

    Time: 6 ms (37.14%), Space: 96.8 MB (28.52%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+     public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast !=null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    public ListNode reverse(ListNode head){
+   if (head == null || head.next == null) {
+    return head;
+}
+            
+        
+        ListNode prev = null;
+        ListNode pres = head;
+        ListNode next = pres.next;
+
+        while(pres != null){
+            pres.next = prev;
+            prev = pres;
+            pres = next;
+            if(next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode headSecond = reverse(mid);
+        ListNode reverseHead = headSecond;
+
+        //compare both the halves
+        while(head != null && headSecond != null){
+            if(head.val != headSecond.val){
+                break;
+            }
+            head =head.next;
+            headSecond = headSecond.next;
+        }
+        reverse(reverseHead);
+
+        if(head == null || headSecond == null){
+            return true;
+        }
+        return false;
+    }
+}
