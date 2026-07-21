75bf6b5b4374da1d5188d7f364c15aab2c770bc7
Gaurav <khanorigaurav37456@gmail.com>
2026-07-21 18:54:46 +0530

    Time: 25 ms (43.7%), Space: 48.2 MB (16.91%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0; i<num.length(); i++){
+            while(!stack.isEmpty() && k >0&& stack.peek() > num.charAt(i)){
+                stack.pop();
+                k--; 
+            }
+            stack.push(num.charAt(i));  
+        }
+        while(k>0){
+            stack.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        while(sb.length()>0 &&sb.charAt(0) == '0'){
+            sb.deleteCharAt(0); 
+        }
+        if(sb.length()==0){
+            return "0";
+        } else{
+         return sb.toString();
+        } 
+    }
+}
