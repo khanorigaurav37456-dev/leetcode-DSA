14b44f4bc1252bc433ef41ac120580d00681e82c
Gaurav <khanorigaurav37456@gmail.com>
2026-08-26 17:22:00 +0530

    Time: 38 ms (8.25%), Space: 47 MB (58.02%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -13,7 +13,7 @@ class Solution {
         for(int i =0; i<s.length(); i++){
             char ch = s.charAt(i);
 
-            if(map.containsKey(ch) && map.get(ch) ==1){
+            if(map.get(ch) ==1){
                 return i;
             }
         }
