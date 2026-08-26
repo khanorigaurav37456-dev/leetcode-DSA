42660c998bcc5b3e90893d8f7fee616e726ab35a
Gaurav <khanorigaurav37456@gmail.com>
2026-08-26 12:27:29 +0530

    Time: 30 ms (33.31%), Space: 125 MB (5.77%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,8 +3,6 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int i =0; i<nums.length; i++){
-            
-
             if(map.containsKey(nums[i])){
                 int previous = map.get(nums[i]);
 
