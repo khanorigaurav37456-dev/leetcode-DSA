e9b8266948c591fa1295d7272adc83d920461cb3
Gaurav <khanorigaurav37456@gmail.com>
2026-09-11 17:51:52 +0530

    Time: 3 ms (87.64%), Space: 52.5 MB (21.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,16 +4,16 @@ class Solution {
     int[] y = {0,0,-1,1};
     public int numIslands(char[][] grid) {
         
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+       
         int n = grid.length;
         int m = grid[0].length;
         
 
         for(int i=0; i<n; i++){
             for(int j =0; j<m; j++){
-                if(grid[i][j] == '1' && !visited[i][j]){
+                if(grid[i][j] == '1'){
                     count++;
-                    dfs(grid,n,m,i,j,visited);
+                    dfs(grid,n,m,i,j);
                 }
             }
         }
@@ -27,14 +27,14 @@ class Solution {
         return true;
     }
 
-    void dfs(char[][] grid,int n, int m, int i, int j, boolean[][] visited){
-        visited[i][j] = true;
+    void dfs(char[][] grid,int n, int m, int i, int j){
+        grid[i][j] = '0';
 
         for(int k=0; k<4; k++){
             int row = i + x[k];
             int col = j + y[k];
-            if(valid(row,col,n,m) && grid[row][col] == '1'&& !visited[row][col] ){
-                dfs(grid,n,m,row,col,visited);
+            if(valid(row,col,n,m) && grid[row][col] == '1' ){
+                dfs(grid,n,m,row,col);
             }
         }
     }
